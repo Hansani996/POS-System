7db5d1191c6861f456db0bd7f5652435f448a62f
hansani996 <hansanimadushika996@gamil.com>
2019-06-16 12:29:00 +0530

    customer add,delete,update

--- BackEnd_POS/src/CustomerServlet.java
+++ BackEnd_POS/src/CustomerServlet.java
@@ -2,6 +2,7 @@ import javax.annotation.Resource;
 import javax.json.Json;
 import javax.json.JsonArrayBuilder;
 import javax.json.JsonObject;
+import javax.json.JsonReader;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -10,9 +11,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 @WebServlet(urlPatterns = "/customer")
 public class CustomerServlet extends HttpServlet {
@@ -56,4 +55,72 @@ public class CustomerServlet extends HttpServlet {
 
         }
     }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        JsonReader reader = Json.createReader(req.getReader());
+        resp.setContentType("application/json");
+        PrintWriter out=resp.getWriter();
+        Connection conn=null;
+
+        try {
+            JsonObject customer =reader.readObject();
+            String id=customer.getString("id");
+            String  name =customer.getString("name");
+            String address=customer.getString("address");
+            conn=ds.getConnection();
+
+            PreparedStatement pstm=conn.prepareStatement("INSERT INTO Customer VALUES(?,?,?) ");
+            pstm.setObject(1,id);
+            pstm.setObject(2,name);
+            pstm.setObject(3,address);
+            boolean result =pstm.executeUpdate()>0;
+
+            if (result){
+                out.println("true");
+            }else {
+                out.println("flase");
+            }
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }finally {
+            try {
+                conn.close();
+            }catch (SQLException e){
+                e.printStackTrace();
+            }
+            out.close();
+        }
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String id = req.getParameter("id");
+
+        if (id != null) {
+
+            try {
+//                Class.forName("com.mysql.jdbc.Driver");
+                Connection connection = ds.getConnection();
+                PreparedStatement pstm = connection.prepareStatement("DELETE FROM Customer WHERE id=?");
+                System.out.println(id);
+                pstm.setObject(1, id);
+                int affectedRows = pstm.executeUpdate();
+                if (affectedRows > 0) {
+                    resp.setStatus(HttpServletResponse.SC_NO_CONTENT);
+                } else {
+                    resp.sendError(HttpServletResponse.SC_NOT_FOUND);
+                }
+            } catch (Exception ex) {
+                resp.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+                ex.printStackTrace();
+            }
+
+        } else {
+            resp.sendError(HttpServletResponse.SC_BAD_REQUEST);
+        }
+    }
+
+
 }
